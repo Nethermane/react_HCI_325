1007f75899bbef952b239626c4caad109e99e799
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-02-28 13:56:07 -0500

    Added pages and routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import login from './pages/login'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <Route path="/login" component={login} />
+        <Route exact path="/" component={App} />
+    </Router>
+)
 
+ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="App">
+            <h1 style={{ color: 'purple' }}>StuPay</h1>
+        </div>
+    );
+}
+
+export default Login;
