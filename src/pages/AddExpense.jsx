102ff32714ec820fb3641d9883bc6542879be680
Chaeme Vogue <17ccv@queensu.ca>
2020-03-09 17:11:47 -0400

    commit

--- src/pages/AddExpense.jsx
+++ src/pages/AddExpense.jsx
@@ -3,62 +3,55 @@ import {Button, Form, Dropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 function AddExpense() {
-  //return <p>add expense page</p>;
-  return (
-      <div style={{ position: "absolute",top: "20%", left: "50%",transform: "translate(-50%, 50%)"}}>
-        <div>
-          <h1>New Expense</h1>
-          <Form>
-            <Form.Group controlId="addNewExpenseName">
-              <Form.Control type="text" placeholder="Netflix" />
-            </Form.Group>
-            <Form.Group controlId="addNewExpenseAmount">
-              <Form.Control type="text" placeholder="$0" />
-            </Form.Group>
-            <label>Frequency</label>
-            <select value="Radish">
-              <option value="Orange">Orange</option>
-              <option value="Radish">Radish</option>
-              <option value="Cherry">Cherry</option>
-            </select>
+    //return <p>add expense page</p>;
+    return (
+        <div style={{margin: "60px"}}>
+            <h1>New Expense</h1>
+            <Form>
+                <Form.Group controlId="addNewExpenseName">
+                    <Form.Control type="text" placeholder="Netflix"/>
+                </Form.Group>
+                <Form.Group controlId="addNewExpenseAmount">
+                    <Form.Control type="text" placeholder="$0"/>
+                </Form.Group>
+                <div>
+                  <Form.Control as="select" style={{width: "100%", marginBottom: "10px", marginTop: "20px"}}>
+                        <option value="" disabled selected hidden>Frequency</option>
+                        <option value="onetime">One Time</option>
+                        <option value="weekly">Weekly</option>
+                        <option value="monthly">Monthly</option>
+                        <option value="yearly">Yearly</option>
+                  </Form.Control>
+                </div>
 
-            <div class={"dropdown"}>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  Frequency
-                </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">One Time</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Monthly</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Yearly</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
+                <div>
+                  <Form.Control as="select" style={{width: "100%", marginBottom: "20px"}}>
+                        <option value="" disabled selected hidden>Category</option>
+                        <option value="necessity">Necessity</option>
+                        <option value="entertainment">Entertainment</option>
+                        <option value="school supplies">School Supplies</option>
+                        <option value="transportation">Transportation</option>
+                  </Form.Control>
+                </div>
+                <div>
+                    <Form.Group controlId="addNewStartDate">
+                        <Form.Control type="text" placeholder="Start Date"/>
+                    </Form.Group>
+                </div>
+                <div>
+                    <Form.Group controlId="addNewDuration">
+                        <Form.Control type="text" placeholder="Duration (Weeks)"/>
+                    </Form.Group>
+                </div>
 
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Category
-              </Dropdown.Toggle>
+            </Form>
 
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">One Time</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Monthly</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Yearly</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-
-          </Form>
-
-          <Link className="navItem" to="/home">
-            <Button variant="secondary">Save</Button>
-          </Link>
+            <Link className="navItem" to="/home">
+                <Button variant="secondary" type="submit">Save</Button>
+            </Link>
         </div>
-      </div>
-  );
-
+    );
 
 
 }
--- src/pages/AddIncome.jsx
+++ src/pages/AddIncome.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {Button, Form} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 function AddIncome() {
-  return <p>add income page</p>;
+  return (
+      <div style={{margin: "60px"}}>
+        <h1>New Income</h1>
+        <Form>
+          <Form.Group controlId="addNewIncomeName">
+            <Form.Control type="text" placeholder="Income Name"/>
+          </Form.Group>
+          <Form.Group controlId="addNewIncomeAmount">
+            <Form.Control type="text" placeholder="Income Amount ($0)"/>
+          </Form.Group>
+          <div>
+            <Form.Group controlId="addNewStartDate">
+              <Form.Control type="text" placeholder="Start Date"/>
+            </Form.Group>
+          </div>
+          <div>
+            <Form.Control as="select" style={{width: "100%", marginBottom: "10px"}}>
+              <option value="" disabled selected hidden>Frequency</option>
+              <option value="onetime">One Time</option>
+              <option value="weekly">Weekly</option>
+              <option value="monthly">Monthly</option>
+              <option value="yearly">Yearly</option>
+              </Form.Control>
+          </div>
+
+
+        </Form>
+
+        <Link className="navItem" to="/home">
+          <Button variant="secondary" type="submit">Save</Button>
+        </Link>
+      </div>
+  );
+
 }
+
 
 export default AddIncome;
