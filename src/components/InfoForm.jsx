5ce1522d4c77c4f2d47955e700871543610a91f9
bellazhong1029 <bellaaa.zjh@gmail.com>
2020-03-27 00:31:04 -0400

    added remove button to page cashflows

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import "./Form.css";
+import "./Cashflow.css";
+import { Container, Col, Row } from "react-bootstrap";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useGlobalState from "../UseGlobalState";
 
 function InfoForm(props) {
+  const state = useGlobalState();
+  const { name, amount, frequency, date, id } = props.data;
   return (
-    <div className="info">
-      <div className="type">{props.type}</div>
-      <div className="amount"> ${props.amount} </div>
+    <div className="cashflow-row">
+      <Container>
+        <Row>
+          <Col>
+            <div className="name cf-item">{name}</div>
+          </Col>
+          <Col>
+            <div className="amount cf-item">{amount}$</div>
+          </Col>
+          <Col>
+            <div className="frequency cf-item">{frequency}</div>
+          </Col>
+          <Col>
+            <div className="date cf-item">{date}</div>
+          </Col>
+          <button
+            onClick={() => {
+              var temp = props.existingVals.filter(element => element.id != id);
+              props.type == "goal" && state.setGoals(temp);
+              props.type == "income" && state.setIncomes(temp);
+              props.type == "expense" && state.setExpenses(temp);
+            }}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/pages/CashFlow.jsx
+++ src/pages/CashFlow.jsx
@@ -1,51 +1,71 @@
-import React from "react";
+import React, { Fragment } from "react";
 import InfoForm from "../components/InfoForm";
 import "./CashFlow.css";
 import useGlobalState from "../UseGlobalState";
+import { Container, Col, Row } from "react-bootstrap";
 
 function CashFlow() {
   const state = useGlobalState();
-  var accountsHtml = [];
+  var goalsHtml = [];
   var incomesHtml = [];
   var expensesHtml = [];
 
-  for (var a in state.accounts) {
-    accountsHtml.push(
+  var existingGoals = [];
+  var existingIncomes = [];
+  var existingExpenses = [];
+
+  // for (var g in state.goals) {
+  //   existingGoals.push(state.goals[g]);
+  //   goalsHtml.push(
+  //     <InfoForm
+  //       key={state.goals[g].id}
+  //       data={state.goals[g]}
+  //       existingVals={existingGoals}
+  //       type="goal"
+  //     />
+  //   );
+  // }
+
+  for (var i in state.incomes) {
+    existingIncomes.push(state.incomes[i]);
+    incomesHtml.push(
       <InfoForm
-        type={state.accounts[a].name}
-        amount={state.accounts[a].amount}
+        key={state.incomes[i].id}
+        data={state.incomes[i]}
+        existingVals={existingIncomes}
+        type="income"
       />
     );
   }
 
-  for (var i in state.incomes) {
-    incomesHtml.push(
-      <InfoForm type={state.incomes[i].name} amount={state.incomes[i].amount} />
-    );
-  }
-
   for (var e in state.expenses) {
+    existingExpenses.push(state.expenses[e]);
     expensesHtml.push(
       <InfoForm
-        type={state.expenses[e].name}
-        amount={state.expenses[e].amount}
+        key={state.expenses[e].id}
+        data={state.expenses[e]}
+        existingVals={existingExpenses}
+        type="expense"
       />
     );
   }
 
   return (
     <div className="cashflows">
-      <div className="accounts">
-        <h>Accounts</h>
-        {accountsHtml}
-      </div>
+      {/* <div className="goals">
+        <p>Goals</p>
+        {goalsHtml}
+        {existingGoals.length == 0 && <div>go add some goals!</div>}
+      </div> */}
       <div className="re-income">
-        <h>Reoccuring Income</h>
+        <p>Recuring Income</p>
         {incomesHtml}
+        {existingIncomes.length == 0 && <div>go set up your incomes!</div>}
       </div>
       <div className="re-expense">
-        <h>Reoccuring Expenses</h>
+        <p>Recuring Expenses</p>
         {expensesHtml}
+        {existingExpenses.length == 0 && <div>go set up your expenses!</div>}
       </div>
     </div>
   );
