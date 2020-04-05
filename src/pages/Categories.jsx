70c323c6975105021d71f94159efe45ca9123714
Bruce Nishimura <16ban1@caslab.queensu.ca>
2020-04-05 16:48:51 -0400

    Fixed category removal

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import useGlobalState from "../UseGlobalState";
 import InfoForm from "../components/InfoForm";
 import { Button, Form, Dropdown } from "react-bootstrap";
-
+function buttonRemove(id, state) {
+    var i = 0;
+    var existingVal = []
+    while (i in state.categories) {
+        if (state.categories[i].id != id) {
+            existingVal.push({ id: state.categories[i].id, name: state.categories[i].name, max: state.categories[i].max })
+        }
+        i++;
+    }
+    state.setCategories(existingVal)
+}
 function Categories() {
     const state = useGlobalState()
     var existingCat = []
     var i = 0;
     var existingVal = []
     while (i in state.categories) {
+        const id = state.categories[i].id
         existingVal.push({ id: state.categories[i].id, name: state.categories[i].name, max: state.categories[i].max })
         existingCat.push(
             <tr>
                 <td>{state.categories[i].name}</td>
                 <td>${state.categories[i].max}</td>
-                <td><button onClick={() => {
-                    var temp = existingVal.filter(e => e.id != existingVal[i - 1].id)
-                    state.setCategories(temp)
-                }
-                }>&#10060;</button></td>
+
+                <td><button id={id} onClick={() => buttonRemove(id, state)}>&#10060;</button></td>
             </tr>
         )
         i++;
@@ -41,7 +49,7 @@ function Categories() {
                         name: document.getElementById("category").value,
                         max: document.getElementById("max").value
                     }, ...existingVal])
-                    
+
                 }}>Submit</Button>
             </Form>
             <table style={{ width: "60%", margin: "auto", marginTop: "10%" }}>
