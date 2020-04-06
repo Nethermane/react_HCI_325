19c8486922bb8ad79c157d6aa0a6a556cc92e435
bellazhong1029 <bellaaa.zjh@gmail.com>
2020-04-06 18:47:04 -0400

    updated cashflows page

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -8,6 +8,32 @@ import useGlobalState from "../UseGlobalState";
 function InfoForm(props) {
   const state = useGlobalState();
   const { name, amount, frequency, date, id } = props.data;
+  const temp = [];
+
+  const onClick = () => {
+    // var temp = props.existingVals.filter((element) => element.id != id);
+    for (var element in props.existingVals) {
+      if (props.existingVals[element].id == id) {
+        var today = new Date();
+        var current_year = today.getFullYear();
+        var current_month = today.getMonth() + 1;
+        var current_day = today.getDate();
+        if (current_month < 10) {
+          var current = current_year + "-0" + current_month + "-" + current_day;
+        } else {
+          var current = current_year + "-" + current_month + "-" + current_day;
+        }
+        props.existingVals[element].end_date = current;
+
+        temp.push(props.existingVals[element]);
+      } else {
+        temp.push(props.existingVals[element]);
+      }
+    }
+    props.type == "income" && state.setIncomes(temp);
+    props.type == "expense" && state.setExpenses(temp);
+  };
+
   return (
     <div className="cashflow-row">
       <Container>
@@ -24,14 +50,7 @@ function InfoForm(props) {
           <Col>
             <div className="date cf-item">{date}</div>
           </Col>
-          <button
-            onClick={() => {
-              var temp = props.existingVals.filter(element => element.id != id);
-              props.type == "goal" && state.setGoals(temp);
-              props.type == "income" && state.setIncomes(temp);
-              props.type == "expense" && state.setExpenses(temp);
-            }}
-          >
+          <button onClick={onClick}>
             <FontAwesomeIcon icon={faTimes} />
           </button>
         </Row>
--- src/pages/CashFlow.jsx
+++ src/pages/CashFlow.jsx
@@ -4,14 +4,6 @@ import "./CashFlow.css";
 import useGlobalState from "../UseGlobalState";
 import { Container, Col, Row } from "react-bootstrap";
 
-var today = new Date();
-var current_year = today.getFullYear();
-var current_month = today.getMonth() + 1;
-//first day of the month
-var firstDay = new Date(current_year, current_month - 1, 1).toLocaleString();
-//last dat of the month
-var lastDay = new Date(current_year, current_month, 0).toLocaleString();
-
 function CashFlow() {
   const state = useGlobalState();
   var incomesHtml = [];
@@ -21,7 +13,15 @@ function CashFlow() {
 
   for (var i in state.incomes) {
     existingIncomes.push(state.incomes[i]);
-    if (state.incomes[i].frequency != "onetime") {
+    var today = new Date();
+    var current_year = today.getFullYear();
+    var current_month = today.getMonth() + 1;
+    var current_day = today.getDate();
+    var current = current_year + "-" + current_month + "-" + current_day;
+    if (
+      state.incomes[i].frequency != "onetime" &&
+      state.incomes[i].end_date > current
+    ) {
       incomesHtml.push(
         <InfoForm
           key={state.incomes[i].id}
@@ -35,7 +35,15 @@ function CashFlow() {
 
   for (var e in state.expenses) {
     existingExpenses.push(state.expenses[e]);
-    if (state.expenses[e].frequency != "onetime") {
+    var today = new Date();
+    var current_year = today.getFullYear();
+    var current_month = today.getMonth() + 1;
+    var current_day = today.getDate();
+    var current = current_year + "-" + current_month + "-" + current_day;
+    if (
+      state.expenses[e].frequency != "onetime" &&
+      state.expenses[e].end_date > current
+    ) {
       expensesHtml.push(
         <InfoForm
           key={state.expenses[e].id}
@@ -58,39 +66,68 @@ function CashFlow() {
         <AmountDisplayCard name="balance" total_amount={total_balance} />
       </div>
       <div className="re-income">
-        <p>Recuring Income +</p>
+        <p>Recuring Incomes +</p>
         {incomesHtml}
-        {existingIncomes.length == 0 && <div>go set up your incomes!</div>}
+        {incomesHtml.length == 0 && <div>go set up your incomes!</div>}
       </div>
       <div className="re-expense">
         <p>Recuring Expenses -</p>
         {expensesHtml}
-        {existingExpenses.length == 0 && <div>go set up your expenses!</div>}
+        {expensesHtml.length == 0 && <div>go set up your expenses!</div>}
       </div>
     </div>
   );
 }
 
-function getSum(data_getsum) {
+function getSum(list) {
   var total_amount = 0;
-  for (var e in data_getsum) {
-    var frequency = data_getsum[e].frequency;
-    var amount = parseInt(data_getsum[e].amount);
-    var date = data_getsum[e].date;
+  var today = new Date();
+  var gs_current_year = today.getFullYear();
+  var gs_current_month = today.getMonth() + 1;
+  var gs_current_day = today.getDate();
+  var gs_current =
+    gs_current_year + "-" + gs_current_month + "-" + gs_current_day;
+  for (var e in list) {
+    var frequency = list[e].frequency;
+    var amount = parseInt(list[e].amount);
+    var date = list[e].date;
     var year = parseInt(date.slice(0, 4));
     var month = parseInt(date.slice(5, 7));
+    var end_date = list[e].end_date;
     if (frequency == "monthly") {
-      total_amount +=
-        amount * (12 * (current_year - year) - month + 1 + current_month);
+      if (end_date > gs_current) {
+        total_amount +=
+          amount *
+          (12 * (gs_current_year - year) - month + 1 + gs_current_month);
+      } else {
+        total_amount +=
+          amount *
+          (12 * (parseInt(end_date.slice(0, 4)) - year) -
+            month +
+            parseInt(end_date.slice(5, 7)));
+      }
     } else if (frequency == "weekly") {
-      total_amount +=
-        amount * 4 * (12 * (current_year - year) - month + 1 + current_month);
+      if (end_date > gs_current) {
+        total_amount +=
+          amount *
+          4 *
+          (12 * (gs_current_year - year) - month + 1 + gs_current_month);
+      } else {
+        total_amount +=
+          amount *
+          4 *
+          (12 * (parseInt(end_date.slice(0, 4)) - year) -
+            month +
+            parseInt(end_date.slice(5, 7)));
+      }
     } else if (frequency == "yearly") {
-      total_amount += (current_year - year + 1) * amount;
+      if (end_date > gs_current) {
+        total_amount += (gs_current_year - year + 1) * amount;
+      } else {
+        total_amount += (parseInt(end_date.slice(0, 4)) - year) * amount;
+      }
     } else {
-      // if (date <= firstDay && date >= lastDay) {
       total_amount += amount;
-      // }
     }
   }
   return total_amount;
